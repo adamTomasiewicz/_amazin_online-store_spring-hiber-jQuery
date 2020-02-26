a4a2cd070acc2f0648c31edb59183786ed949770
adamTomasiewicz <adam.tomasiewicz@outlook.com>
2020-02-26 16:37:10 +0100

    thymeleaf gallery and crud tables

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,30 @@
-const $addProduct = $('a');
-const $updateProduct = $('b');
-const $deleteProduct = $btn.find('c');
+// const $addProduct = $('a');
+// // const $updateProduct = $('b');
+// // const $deleteProduct = $btn.find('c');
+$(document).ready(function() {
+    $btn.on('click', function() {
+        $users.html('');
+        $btn.attr('disabled', true);
+        $spinner.show();
+
+        $.ajax('https://jsonplaceholder.typicode.com/users')
+            .done(function (users) {
+                users.forEach(function(user) {
+                    $users.append(`
+                        <tr>
+                            <td>${user.name}<td>
+                            <td>${user.username}<td>
+                            <td>${user.email}<td>
+                        </tr>
+                    `);
+                });
+            })
+            .fail(function() {
+                alert('Spróbuj ponownie')
+            })
+            .always(function() {
+                $btn.attr('disabled', false);
+                $spinner.hide();
+            });
+    })
+}
